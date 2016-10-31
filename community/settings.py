c0130559c98c738b3c48ecf1ef7799c5b14df76f
b-porter <beneporter@gmail.com>
2016-10-31 12:07:34 -0700

    rename staticfiles dir to static

--- community/settings.py
+++ community/settings.py
@@ -144,7 +144,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Extra places for collectstatic to find staticfiles files.
