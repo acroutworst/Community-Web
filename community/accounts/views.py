230e9a5ee14d5cc0b8912207861fa0f92b3430d9
b-porter <beneporter@gmail.com>
2016-11-01 09:54:43 -0700

    update profile/view

--- community/accounts/views.py
+++ community/accounts/views.py
@@ -3,17 +3,23 @@ from django.forms import modelformset_factory
 from django.shortcuts import render
 from .models import UserProfile
 from .forms import ProfileEditForm
+from allauth.account.models import EmailAddress
 from django.http import HttpResponseRedirect
 
 @login_required
 def profile_view(request):
     user = request.user
     profileQuery = UserProfile.objects.filter(user=user)
+    emailQuery = EmailAddress.objects.filter(user=user)
     if profileQuery.count() == 0:
         profile = UserProfile.objects.create(user=user)
     else:
         profile = UserProfile.objects.get(user=user)
-    context = {'profile': profile}
+    context = {
+        'profile': profile,
+        'user': user,
+        'emails': emailQuery,
+    }
     return render(request, 'accounts/profile/view.html', context)
 
 @login_required
