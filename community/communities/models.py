1bf90068d5365958a2f539628ed6e4c9d69805e4
b-porter <beneporter@gmail.com>
2016-11-03 23:17:58 -0700

    add model Community

--- community/communities/models.py
+++ community/communities/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Community(models.Model):
+    title = models.CharField(max_length=256, blank=False, null=False)
+    phone_number = models.CharField(max_length=10, blank=True, null=True)
+    location = models.CharField(max_length=40, blank=True, null=True)
+    description = models.CharField(max_length=512, blank=True, null=True)
