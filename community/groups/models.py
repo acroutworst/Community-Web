bdca3a9b3ff0f4c5b5851fe5bea5fa9e3bf3c917
xiaoyaowuming <pengfz@uw.edu>
2016-11-03 17:47:37 -0700

    Update for Group Model'

--- community/groups/models.py
+++ community/groups/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+
 # Create your models here.
+class Group(models.Model):
+    CommunityID = models.CharField(max_length=30, blank=True, null=True)
+    title = models.CharField (max_length = 30, blank = True, null = True)
+    description = models.CharField(max_length=30, blank=True, null=True)
+    groupDate = models.CharField(max_length=30, blank=True, null=True)
+    LeaderName = models.CharField(max_length=30, blank=True, null=True)
+    GroupID = models.CharField(max_length=30, blank=True, null=True)
+
+    def __str__(self):
+        return self.title
