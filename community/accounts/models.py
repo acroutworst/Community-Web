a89727059d4497b47b22b322899690a211e5b740
b-porter <beneporter@gmail.com>
2016-11-13 16:05:38 -0800

    bugfix

--- community/accounts/models.py
+++ community/accounts/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from annoying.fields import AutoOneToOneField
 import os
 
 
--- community/accounts/templatetags/profile_tags.py
+++ community/accounts/templatetags/profile_tags.py
@@ -1,9 +1,7 @@
 from django import template
 from community.accounts.models import Profile, ProfileImage
-from django.contrib.auth.models import User
 from django.conf import settings
 from django.utils.html import format_html
-from sorl.thumbnail import get_thumbnail
 import os
 
 register = template.Library()
@@ -12,8 +10,8 @@ register = template.Library()
 def avatar(user):
     profile = Profile.objects.get(user=user)
     try:
-        avatar = ProfileImage.objects.get(profile=profile)
-        image = avatar.image.url
-    except ProfileImage.DoesNotExist:
+        image = profile.image.image.url
+    except:
         image = os.path.join(settings.MEDIA_URL, 'accounts/profiles/default.jpg')
+
     return format_html('''<img src="{}">''', image)
