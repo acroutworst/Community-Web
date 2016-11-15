e853f52e6b660ba8c4a060d528c42fee70b30d8c
shayan raouf <shayanraouf@gmail.com>
2016-11-15 12:04:13 -0800

    add basic html files for event home page and create event

--- community/events/views.py
+++ community/events/views.py
@@ -2,5 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('<p>Events Main Page</p>')
+    return render(request,template_name='events/home.html')
+    #return HttpResponse('<p>Events Main Page</p>')
 
