c49793f07e718c584ef5b341a5313c11517e839f
xiaoyaowuming <pengfz@uw.edu>
2016-11-15 15:59:47 -0800

    update for MVC of groups

--- community/groups/urls.py
+++ community/groups/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
 
+
 from . import views
 
 urlpatterns = [
-    url(r'^list', views.list_groups, name='list groups'),
+    url(r'^$', views.groups_list, name="groups_list"),
+    url(r'^(?P<id>[\d]+)/$', views.group_view, name = "groups_view"),
 ]
