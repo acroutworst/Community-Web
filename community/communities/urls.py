185e56f2c6cc0e7e483edfa5e356f8628948ad09
acroutworst <croutworsta@gmail.com>
2016-11-15 18:38:33 -0800

    added files moving to master

--- community/communities/urls.py
+++ community/communities/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.communities_list, name="communities_list"),
-    url(r'^(?P<slug>[\w-]+)/$', views.communities_view, name="communities_view"),
+        url(r'^(?P<slug>[\w-]+)/$', views.communities_view, name="communities_view"),
     url(r'^(?P<slug>[\w-]+)/join/$', views.communities_join, name="communities_join"),
     url(r'^(?P<slug>[\w-]+)/deactivate/$', views.communities_deactivate, name="communities_deactivate"),
     url(r'^(?P<slug>[\w-]+)/meetups/', include('community.meetups.urls')),
--- community/views.py
+++ community/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from django.http import HttpResponseRedirect
+from .communities.models import Community, CommunityUserProfile
 
 
 def home(request):
@@ -15,7 +16,11 @@ def home(request):
 
 @login_required
 def home_login(request):
+    all_communities = Community.objects.all()
+    my_communities = Community.objects.filter(communityuserprofile__user=request.user)
     context = {
         'user': request.user,
+        'all_communities': all_communities,
+        'my_communities': my_communities,
     }
     return render(request, template_name='index_user.html', context=context)
