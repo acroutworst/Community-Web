4e1ce65c6f0c741ed14cf83cd112e09420851671
xiaoyaowuming <pengfz@uw.edu>
2016-11-22 17:35:42 -0800

    Update the view of communityUserProfile

--- community/communities/urls.py
+++ community/communities/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     url(r'^(?P<slug>[\w-]+)/join/$', views.communities_join, name="communities_join"),
     url(r'^(?P<slug>[\w-]+)/deactivate/$', views.communities_deactivate, name="communities_deactivate"),
     url(r'^(?P<slug>[\w-]+)/meetups/', include('community.meetups.urls')),
+    url(r'^(?P<slug>[\w-]+)/profile/',views.communities_profile, name = "communities_profile"),
 
 ]
