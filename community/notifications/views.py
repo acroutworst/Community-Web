500ceb84e4e5774151635be44ffaa0aaae3931fd
ignatiusbilly <ignatiusbill@gmail.com>
2016-11-27 13:04:53 -0800

    notifications in development

---
+++ community/notifications/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+from .models import Notification
+
+# Create your views here.
+def notifications_view(request):
+    notifications = Notification.objects.order_by('date')[:10]
+    context = {
+        'notifications_list': notifications
+    }
+    return render(request, 'notifications/view.html', context)
