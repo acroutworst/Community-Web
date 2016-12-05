638ff95c6d44bd52c60f6dab86f315bd66130ed8
shayan raouf <shayanraouf@gmail.com>
2016-12-04 21:25:59 -0800

    fixed the events and views page

--- community/events/urls.py
+++ community/events/urls.py
@@ -5,7 +5,8 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name="events_index"),
     url(r'create_events/', views.create_events, name="create_events"),
-    url(r'view_event/', views.view_event, name="view_event"),
+    #url(r'view_event/', views.view_event, name="view_event"),
+    url(r'^events/(?P<event_id>[0-9]+)/$', views.view_event, name="view_event"),
 ]
 # url(r'^clubs/(?P<club_id>[\w]+)/events/create$', EventCreateView.as_view(), name='event_create'),
 #     url(r'^clubs/(?P<club_id>[\w]+)/events/(?P<event_id>[0-9]+)/$', EventView.as_view()),
