ec65a69bd6dc269ec6016bc08217a81a3bbc48ef
xiaoyaowuming <pengfz@uw.edu>
2016-12-13 19:46:01 -0800

    update the create group form and show memeber join data on member page

--- community/groups/form.py
+++ community/groups/form.py
@@ -2,7 +2,6 @@ from django import forms
 from .models import Group
 
 class CreateGroupForm(forms.ModelForm):
-    name = forms.CharField(required=True)
     title = forms.CharField(required=True)
     description = forms.CharField()
     #current_leader = forms.CharField()
@@ -10,7 +9,7 @@ class CreateGroupForm(forms.ModelForm):
     class Meta:
         model = Group
         #fields = ['name', 'title', 'description', 'current_leader']
-        fields = ['name', 'title', 'description']
+        fields = ['title', 'description']
 
 #class JoinGroupForm(forms.ModelForm):
     #class Meta:
