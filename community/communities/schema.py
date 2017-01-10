d1c0f6648cb1019a29fce194ab3f53943b0b7a50
b-porter <beneporter@gmail.com>
2017-01-10 00:01:48 -0800

    add graphene mutation for registering communities

--- community/communities/schema.py
+++ community/communities/schema.py
@@ -1,7 +1,11 @@
 from .models import Community as CommunityModel, CommunityUserProfile as ProfileModel
 from graphene_django.types import DjangoObjectType
 from graphene_django.filter.fields import DjangoFilterConnectionField
-from graphene import AbstractType, Node
+from graphene import AbstractType, Node, Mutation
+from django.contrib.auth.models import User
+from django.utils import timezone
+import graphene
+from graphql_relay.node.node import from_global_id
 
 
 class CommunityNode(DjangoObjectType):
@@ -10,6 +14,33 @@ class CommunityNode(DjangoObjectType):
         filter_fields = ['title', 'acronym', 'creator', 'date_created', 'slug']
         interfaces = (Node,)
 
+class RegisterCommunity(Mutation):
+    class Input:
+        title = graphene.String()
+        acronym = graphene.String()
+        phone_number = graphene.String()
+        description = graphene.String()
+        creator = graphene.ID()
+        slug = graphene.String()
+
+    ok = graphene.Boolean()
+    community = graphene.Field(lambda: CommunityNode)
+
+    def mutate(self, args, context, info):
+        creator_id = from_global_id(args.get('creator'))[1]
+        print(creator_id)
+        community = CommunityModel(
+            title=args.get('title'),
+            acronym=args.get('acronym'),
+            phone_number=args.get('phone_number'),
+            description=args.get('description'),
+            creator=User.objects.get(id=creator_id),
+            date_created=timezone.now(),
+            slug=args.get('slug'),
+        )
+        community.save()
+        ok = True
+        return RegisterCommunity(community=community, ok=ok)
 
 class CommunityUserProfileNode(DjangoObjectType):
     class Meta:
@@ -23,3 +54,6 @@ class Query(AbstractType):
     all_communities = DjangoFilterConnectionField(CommunityNode)
     community_user_profile = Node.Field(CommunityUserProfileNode)
     all_community_user_profiles = DjangoFilterConnectionField(CommunityUserProfileNode)
+
+class Mutation(AbstractType):
+    register_community = RegisterCommunity.Field()
--- community/schema.py
+++ community/schema.py
@@ -4,41 +4,35 @@ import community.accounts.schema
 from graphene_django.types import DjangoObjectType
 from graphene_django.filter.fields import DjangoFilterConnectionField
 from graphene import AbstractType, Node, ObjectType
-from django.contrib.auth.models import User as UserModel
+import django.contrib.auth
 import graphene
 from graphene_django.debug import DjangoDebug
-from graph_auth.schema import Query as AuthQuery, Mutation as AuthMutation
+from graph_auth.schema import Query as AuthQuery, Mutation as AuthMutation, UserNode as AuthUserNode
 
 
-# class UserNode(DjangoObjectType):
-#     class Meta:
-#         model = UserModel
-#         filter_fields = ['id', 'email']
-#         interfaces = (Node,)
-#
-#     @classmethod
-#     def get_node(cls, id, context, info):
-#         user = super(UserNode, cls).get_node(id, context, info)
-#         if context.user.id and user.id == context.user.id:
-#             return user
-#         else:
-#             return None
-#
-# class UserQuery(AbstractType):
-#
-#     user = Node.Field(UserNode)
-#     all_users = DjangoFilterConnectionField(UserNode)
-#     me = graphene.Field(UserNode)
-#
-#     def resolve_me(self, args, context, info):
-#         UserNode.get_node(context.user.id, context, info)
+class CommunityUserNode(AuthUserNode):
+    class Meta:
+        model = django.contrib.auth.get_user_model()
+        filter_fields = ['id', 'email']
+        interfaces = (Node,)
+
+    # @classmethod
+    # def get_node(cls, id, context, info):
+    #     user = super(UserNode, cls).get_node(id, context, info)
+    #     if context.user.id and user.id == context.user.id:
+    #         return user
+    #     else:
+    #         return None
+
+class UserQuery(AuthQuery):
+    all_users = DjangoFilterConnectionField(CommunityUserNode)
 
 # UserQuery,
 # community.meetups.schema.Query,
 # community.communities.schema.Query,
 # community.accounts.schema.Query,
 
-class Query(AuthQuery,
+class Query(UserQuery,
             community.communities.schema.Query,
             community.meetups.schema.Query,
             community.accounts.schema.Query,
@@ -46,7 +40,7 @@ class Query(AuthQuery,
     debug = graphene.Field(DjangoDebug, name='__debug')
 
 
-class Mutation(AuthMutation, ObjectType):
+class Mutation(AuthMutation, community.communities.schema.Mutation, ObjectType):
     pass
 
 schema = graphene.Schema(query=Query, mutation=Mutation)
