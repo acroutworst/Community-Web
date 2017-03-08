a0fc570810072a26758c0cc4654d197c60530fd7
b-porter <beneporter@gmail.com>
2017-03-08 00:14:17 -0800

    add community posts

--- community/communities/admin.py
+++ community/communities/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Community, CommunityUserProfile
+from .models import Community, CommunityUserProfile, Post, PostImage
 
 # Register your models here.
 admin.site.register(Community)
 admin.site.register(CommunityUserProfile)
+admin.site.register(Post)
+admin.site.register(PostImage)
